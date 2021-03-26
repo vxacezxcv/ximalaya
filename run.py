da8a90924cfc9098a358744c1b35b0993125d94a
Bowen Lu <Bowen.Lu@mail.com>
2021-03-26 23:15:58 +0800

    pylint

--- run.py
+++ run.py
@@ -15,18 +15,23 @@ from urllib import parse
 
 import requests
 
-#备用方案1
-#关键字搜索1https://www.ximalaya.com/revision/album/v1/getTracksList?albumId={}&pageNum=1
-#获取下载链接1https://www.ximalaya.com/revision/play/v1/audio?id={搜索1ID}&ptype=1
-#获取下载链接2 http://mobwsa.ximalaya.com/mobile/playlist/album/page?albumId={}&pageId=1
-#关键字搜索http://searchwsa.ximalaya.com/front/v1?appid=0&condition=relation&core=chosen2&device=android&deviceId=9a68144e-de5b-3c60-be5e-adce947ab5ff&kw={}&live=true&needSemantic=true&network=wifi&operator=1&page=1&paidFilter=false&plan=c&recall=normal&rows=20&search_version=2.8&spellchecker=true&version=6.6.48&voiceAsinput=false
-columns1 = ("TITTLE","ID")
-headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.96 Safari/537.36'}
+# 备用方案1
+# 关键字搜索1 https://www.ximalaya.com/revision/album/v1/getTracksList?albumId={}&pageNum=1
+# 获取下载链接1 https://www.ximalaya.com/revision/play/v1/audio?id={搜索1ID}&ptype=1
+# 获取下载链接2 http://mobwsa.ximalaya.com/mobile/playlist/album/page?albumId={}&pageId=1
+# 关键字搜索 http://searchwsa.ximalaya.com/front/v1?appid=0&condition=relation&core=chosen2&device=android&deviceId=9a68144e-de5b-3c60-be5e-adce947ab5ff&kw={}&live=true&needSemantic=true&network=wifi&operator=1&page=1&paidFilter=false&plan=c&recall=normal&rows=20&search_version=2.8&spellchecker=true&version=6.6.48&voiceAsinput=false
+columns1 = ('TITTLE', 'ID')
+headers = {'user-agent': ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                          'AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/72.0.3626.96 Safari/537.36')}
+
 def xm_md5():
     url = 'https://www.ximalaya.com/revision/time'
-    headrer = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.96 Safari/537.36',
-            'Host': 'www.ximalaya.com',
-            'Accept-Encoding': 'gzip, deflate, br'}
+    headrer = {'User-Agent': ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                              'AppleWebKit/537.36 (KHTML, like Gecko) '
+                              'Chrome/72.0.3626.96 Safari/537.36'),
+               'Host': 'www.ximalaya.com',
+               'Accept-Encoding': 'gzip, deflate, br'}
     try:
         html = requests.get(url, headers = headrer)
         nowTime = str(round(time.time()*1000))
@@ -35,6 +40,8 @@ def xm_md5():
         tkinter.messagebox.showerror('错误','请检查网络是否畅通')
         return 0
     return sign
+
+
 def open_link():
     Listbox1.delete(0,END)
     Listbox2.delete(0,END)
@@ -65,6 +72,8 @@ def open_link():
         return 0
     Text1.insert(END, '> 解析线程结束\n')
     Text1.see(END)
+
+
 def download():
     global path
     xuanzhong_index = Listbox1.curselection()
@@ -79,6 +88,8 @@ def download():
             code.write(file1.content)
         Text1.insert(END, '> ' + file_name + '下载成功\n')
         Text1.see(END)
+
+
 def solve():
     Text1.insert(END, '> 解析线程开始\n')
     Text1.see(END)
@@ -108,10 +119,14 @@ def solve():
         return 0
     Text1.insert(END, '> 解析线程结束\n')
     Text1.see(END)
+
+
 def clear_list(tree):
     x=tree.get_children()
     for item in x:
         tree.delete(item)
+
+
 def search(): #按照关键词进行搜索
     Text1.insert(END, '> 搜索线程开始\n')
     Text1.see(END)
@@ -139,24 +154,38 @@ def search(): #按照关键词进行搜索
         return 0
     Text1.insert(END, '> 搜索线程结束\n')
     Text1.see(END)
+
+
 def set_dir():
     global path
     path = tkinter.filedialog.askdirectory()
     Entry3.delete(0,END)
     Entry3.insert(END,path)
+
+
 def pass_download():
     threading.Thread(target=download).start()
+
+
 def open_link_button_click():
     threading.Thread(target=open_link).start()
+
+
 def treeview1_click(event):
     threading.Thread(target=solve).start()
+
+
 def search_button_click():
     threading.Thread(target=search).start()
+
+
 def set_dir_click():
     threading.Thread(target=set_dir).start()
-#GUI
+
+
+# GUI
 windows = tk.Tk()
-windows.geometry('917x564')# +34+306
+windows.geometry('917x564')  # +34+306
 windows.title('喜马拉雅专辑下载4.2 BY:Snow')
 windows.resizable(0,0)
 Label1 = tk.Label(windows)
@@ -181,17 +210,19 @@ Text1 = tk.Text(windows)
 Text1.place(height = 88,width = 904,x = 5,y = 469)
 Text1.insert(END, '> 启动成功！\n')
 Text1.see(END)
-#列表1
-treeview1 = ttk.Treeview(windows, height=10, show="headings", columns=columns1)
+
+# 列表1
+treeview1 = ttk.Treeview(windows, height=10, show='headings', columns=columns1)
 treeview1.place(height = 299,width = 530,x = 5,y = 116)
-treeview1.column("TITTLE", width=330, anchor='center')  # 表示列,不显示
-treeview1.column("ID", width=200, anchor='center')
-treeview1.heading("TITTLE", text="TITTLE")  # 显示表头
-treeview1.heading("ID", text="ID")
+treeview1.column('TITTLE', width=330, anchor='center')  # 表示列, 不显示
+treeview1.column('ID', width=200, anchor='center')
+treeview1.heading('TITTLE', text='TITTLE')  # 显示表头
+treeview1.heading('ID', text='ID')
 treeview1.bind('<Double-1>', treeview1_click)
 Scrollbar1 = tk.Scrollbar(treeview1)
 Scrollbar1.pack(side=RIGHT, fill=Y)
-#列表2
+
+# 列表2
 Listbox1 = tk.Listbox(windows,selectmode = EXTENDED)
 Listbox1.place(height = 299,width = 370,x = 539,y = 116)
 Scrollbar2 = tk.Scrollbar(Listbox1)
@@ -202,5 +233,7 @@ treeview1.config(yscrollcommand=Scrollbar1.set)
 Listbox1.config(yscrollcommand=Scrollbar2.set)
 Scrollbar1.config(command = treeview1.yview)
 Scrollbar2.config(command = Listbox1.yview)
+
+
 if __name__ == '__main__':
     windows.mainloop()
